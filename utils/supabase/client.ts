b7be9349f60b4d549b0f72dd6c2ee7aefdb2d2dd
paulmwas211 <paulmwas8341@gmail.com>
2025-05-21 03:12:54 +0300

    feat:signup page

--- utils/supabase/client.ts
+++ utils/supabase/client.ts
@@ -1,7 +1,7 @@
-import { createBrowserClient } from '@supabase/auth-helpers-nextjs'
+// utils/supabase.ts
+import { createClient } from '@supabase/supabase-js'
 
-export const createClient = () =>
-  createBrowserClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
-  )
+export const supabase = createClient(
+  process.env.NEXT_PUBLIC_SUPABASE_URL!,
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+)
