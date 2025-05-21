b084093bf62de4f698adedcb26591c7b8e9f575f
paulmwas211 <paulmwas8341@gmail.com>
2025-05-21 12:45:04 +0300

    fix: updated necessary things

--- src/components/SignInWithGoogle.tsx
+++ src/components/SignInWithGoogle.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { Button } from "./ui/button";
+import { Button } from "@/components/ui/button";
 import { FcGoogle } from "react-icons/fc";
 import { useRouter } from "next/navigation";
 import { useEffect } from "react";
--- src/utils/supabase/client.ts
+++ src/utils/supabase/client.ts
@@ -1,7 +1,5 @@
-// src/utils/supabase/client.ts
-import { createBrowserClient } from "@supabase/ssr";
+import { createBrowserClient } from '@supabase/ssr';
 
-// You must set NEXT_PUBLIC_SUPABASE_URL and NEXT_PUBLIC_SUPABASE_ANON_KEY in your `.env.local`
 export const supabase = createBrowserClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL!,
   process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
